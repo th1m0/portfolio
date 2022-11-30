12dd17b24edcac740cb55e162abd63be53ce2566
Thiimo <thimosietsma@gmail.com>
2022-11-30 16:22:20 +0100

    dev

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -11,20 +11,21 @@ type Props = {
 export default function Skill({ skill, directionLeft }: Props) {
   return (
     <div className="group relative flex cursor-pointer">
-      <motion.img
-        initial={{
-          x: directionLeft ? -200 : 200,
-          opacity: 0,
-        }}
-        transition={{
-          duration: 1,
-        }}
-        whileInView={{
-          opacity: 1,
-          x: 0,
-        }}
-        src={urlFor(skill?.image)?.url() ?? ""}
+      <img
+        // initial={{
+        //   x: directionLeft ? -200 : 200,
+        //   opacity: 0,
+        // }}
+        // transition={{
+        //   duration: 1,
+        // }}
+        // whileInView={{
+        //   opacity: 1,
+        //   x: 0,
+        // }}
         className="rounded-full border border-gray-500 object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+        src={urlFor(skill?.image)?.url() ?? ""}
+        alt=""
       />
 
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 rounded-full z-0">
