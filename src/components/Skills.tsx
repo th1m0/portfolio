a39a52055ebd0f9aee7f49a3bff0400b27e84e83
Thimo <thimosietsma@gmail.com>
2025-07-11 23:03:48 +0200

    fix: ci

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -17,9 +17,9 @@ export default function Skills({ skills }: { skills: SkillsQueryResult }) {
       </h3>
 
       <div className="grid grid-cols-4 gap-5">
-        {skills
-          ?.slice(0, skills.length / 2)
-          ?.map((skill) => <Skill key={skill._id} skill={skill} />)}
+        {skills?.slice(0, skills.length / 2)?.map((skill) => (
+          <Skill key={skill._id} skill={skill} />
+        ))}
 
         {skills.slice(skills.length / 2, skills.length).map((skill) => (
           <Skill key={skill._id} skill={skill} />
